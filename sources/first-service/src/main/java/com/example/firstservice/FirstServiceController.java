459611628a64c730808248d5ea9e3d079e70eba2
edowon0623@gmail.com <Dwlee$09>
2021-08-05 17:51:05 +0900

    update apigateway samples

--- sources/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ sources/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,8 +1,12 @@
 package com.example.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @RestController
 @RequestMapping("/first-service")
@@ -12,5 +16,11 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader(value = "first-request", required = true) String header) {
+        System.out.println(header);
+        return "Hello, First service.";
     }
 }
