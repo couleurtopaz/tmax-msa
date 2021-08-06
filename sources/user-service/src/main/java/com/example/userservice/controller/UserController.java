44a548923483f26c7ce2ca1119567d25b5f477da
edowon0623@gmail.com <Dwlee$09>
2021-08-06 15:50:51 +0900

    upload user-service #1

--- sources/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ sources/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,19 +1,28 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.entity.UserEntity;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
     private final Environment env;
+    private final UserService userService;
 
     @Autowired
-    public UserController(Environment env) {
+    public UserController(Environment env, UserService userService) {
         this.env = env;
+        this.userService = userService;
     }
 
     @GetMapping("/health_check")
@@ -27,7 +36,12 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestBody RequestUser user) {
+    public String createUser(@RequestBody @Valid RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
         return "Create user method called";
     }
 }
