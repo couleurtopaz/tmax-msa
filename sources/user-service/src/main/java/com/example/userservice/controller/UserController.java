d34d392e1c131308e17def49fdc1e2d9e07d939c
edowon0623@gmail.com <Dwlee$09>
2021-08-06 14:45:40 +0900

    upload user-service #1

---
+++ sources/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.RequestUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final Environment env;
+
+    @Autowired
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+        return "Create user method called";
+    }
+}
